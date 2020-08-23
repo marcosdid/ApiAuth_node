e9abf4fb7575bdd7eeafb364a44949473f444004
marcosdid <limafilhoguerreiro@outlook.com>
2020-08-23 00:37:59 -0300

    add pre push to husky with jest:ci script

--- src/presentation/routes/login-routes.js
+++ src/presentation/routes/login-routes.js
@@ -32,7 +32,7 @@ module.exports = class LoginRouter {
 
       return HttpResponse.ok({ accessToken })
     } catch (error) {
-      // console.log(error)
+      console.log(error)
       return HttpResponse.serverError()
     }
   }
