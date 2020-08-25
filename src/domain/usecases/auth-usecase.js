39c8c7ff15603d4b154d0e5d99b811b48e60ae09
marcosdid <limafilhoguerreiro@outlook.com>
2020-08-25 20:09:06 -0300

    ensure AuthUseCase returns an accessToken if valid credentials are provided

--- src/domain/usecases/auth-usecase.js
+++ src/domain/usecases/auth-usecase.js
@@ -23,10 +23,12 @@ module.exports = class AuthUseCase {
     }
 
     const isValid = await this.escrypter.compare(password, user.password)
+
     if (!isValid) {
       return null
     }
 
-    await this.tokenGenerator.generate(user.id)
+    const accessToken = await this.tokenGenerator.generate(user.id)
+    return accessToken
   }
 }
--- src/domain/usecases/auth-usecase.spec.js
+++ src/domain/usecases/auth-usecase.spec.js
@@ -122,4 +122,14 @@ describe('Auth UseCase', () => {
     await sut.auth('valid_email@gmail.com', 'valid_password')
     expect(tokenGeneratorSpy.userId).toBe(loadUserByEmailRepositorySpy.user.id)
   })
+
+  test('Should return an accessToken id correct credentials are provided', async () => {
+    const { sut, tokenGeneratorSpy } = makeSut()
+    const accessToken = await sut.auth(
+      'valid_email@gmail.com',
+      'valid_password'
+    )
+    expect(accessToken).toBe(tokenGeneratorSpy.accessToken)
+    expect(accessToken).toBeTruthy()
+  })
 })
