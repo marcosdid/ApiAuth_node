78e42218d4765a3629d8a4c6c8ff41997af597e9
marcosdid <limafilhoguerreiro@outlook.com>
2020-08-29 19:34:04 -0300

    move cors to its own file

--- src/main/config/setup.js
+++ src/main/config/setup.js
@@ -1,9 +1,6 @@
+const cors = require('../middlewares/cors')
+
 module.exports = (app) => {
   app.disable('x-powered-by')
-  app.use((req, res, next) => {
-    res.set('Access-Control-Allow-Origin', '*')
-    res.set('Access-Control-Allow-methods', '*')
-    res.set('Access-Control-Allow-headers', '*')
-    next()
-  })
+  app.use(cors)
 }
---
+++ src/main/middlewares/cors.test.js
@@ -0,0 +1,14 @@
+const request = require('supertest')
+const app = require('../config/app')
+
+describe('cors middleware', () => {
+  test('Should enable CROS', async () => {
+    app.get('/test_cors', (req, res) => {
+      res.send('')
+    })
+    const res = await request(app).get('/test_x_powered_by')
+    expect(res.header['access-control-allow-origin']).toBe('*')
+    expect(res.header['access-control-allow-methods']).toBe('*')
+    expect(res.header['access-control-allow-headers']).toBe('*')
+  })
+})
